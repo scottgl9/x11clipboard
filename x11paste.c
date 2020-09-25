ade37e60afe95710abbe5fc7b51c78902d216c05
scottgl <scottgl@gmail.com>
2020-09-25 10:34:08 -0500

    Updates to x11paste

--- x11paste.c
+++ x11paste.c
@@ -1,36 +1,36 @@
-#include <stdio.h> // printf
-#include <string.h> // strndup
+#include <stdio.h>
+#include <string.h>
 #include <X11/Xlib.h>
-int XA_STRING = 31;
+#include <X11/Xatom.h>
 
 static Display * display;
 static Window window;
 static Atom UTF8; 
 
 char * XPasteType(Atom atom) {
-	XEvent event;
+	XEvent ev;
 	int format;
 	unsigned long N, size;
 	char * data, * s = 0;
-	Atom target,
-		CLIPBOARD = XInternAtom(display, "CLIPBOARD", 0),
-		XSEL_DATA = XInternAtom(display, "XSEL_DATA", 0);
-	XConvertSelection(display, CLIPBOARD, atom, XSEL_DATA, window, CurrentTime);
+	Atom target, clipboard, xsel;
+	clipboard = XInternAtom(display, "CLIPBOARD", 0),
+	xsel = XInternAtom(display, "XSEL_DATA", 0);
+	XConvertSelection(display, clipboard, atom, xsel, window, CurrentTime);
 	XSync(display, 0);
-	XNextEvent(display, &event);
-	
-	switch(event.type) {
+	XNextEvent(display, &ev);
+
+	switch(ev.type) {
 		case SelectionNotify:
-		if(event.xselection.selection != CLIPBOARD) break;
-		if(event.xselection.property) {
-			XGetWindowProperty(event.xselection.display, event.xselection.requestor,
-				event.xselection.property, 0L,(~0L), 0, AnyPropertyType, &target,
+		if(ev.xselection.selection != clipboard) break;
+		if(ev.xselection.property) {
+			XGetWindowProperty(ev.xselection.display, ev.xselection.requestor,
+				ev.xselection.property, 0L,(~0L), 0, AnyPropertyType, &target,
 				&format, &size, &N,(unsigned char**)&data);
 			if(target == UTF8 || target == XA_STRING) {
 				s = strndup(data, size);
 				XFree(data);
 			}
-			XDeleteProperty(event.xselection.display, event.xselection.requestor, event.xselection.property);
+			XDeleteProperty(ev.xselection.display, ev.xselection.requestor, ev.xselection.property);
 		}
 	}
   return s;
@@ -49,6 +49,7 @@ int main(int argc, char *argv[]) {
 	int N = DefaultScreen(display);
 	window = XCreateSimpleWindow(display, RootWindow(display, N), 0, 0, 1, 1, 0,
 		BlackPixel(display, N), WhitePixel(display, N)
-	);	
-	printf("%s\n", XPaste());
+	);
+	char *c = XPaste();
+	if (c != NULL) printf("%s\n", c);
 }
